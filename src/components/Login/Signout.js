4775e33fa5ed4336f7240a14012e5e9f38e7f015
Shannon Silva <Shannon-Silva>
2018-06-15 16:05:02 +0530

    login, register, recover password

---
+++ src/components/Login/Signout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Input, Button, Label, Card, Alert,
+  Modal, ModalHeader, ModalBody, Form, FormGroup
+} from 'reactstrap';
+import { Redirect } from "react-router-dom";
+
+class Signout extends Component{
+render(){
+    
+}
+}
+
+export default Signout;
