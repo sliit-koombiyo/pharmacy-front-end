14b9cf643d244c89a59c053f6a02d791303d46e1
udrathnayaka <rathnayakar07@gmail.com>
2018-06-15 21:43:22 -0700

    n

--- src/components/assistant/SendRequests.js
+++ src/components/assistant/SendRequests.js
@@ -14,63 +14,6 @@ const defaultOption = DropDownFortypes[0];
 class SendRequests extends Component {
 
 
-<<<<<<< HEAD
-    
-    componentDidMount(){
-        axios.get('http://localhost:5000/requests').then((response) => {
-            console.log(JSON.stringify("request list" + JSON.stringify(response.data.data)));
-            this.setState({ requests: response.data.data})
-          });          
-         
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const data = new FormData(event.target); 
-        console.log("form data : " + JSON.stringify(event.target.requestID.value)) 
-        
-    }   
-          
-
-    render() {
-        return (
-          <div>
-            <Card>
-              <CardHeader style={{ backgroundColor: '#397ed0', color: 'white' }}>Send Requests</CardHeader>
-              <CardBody>
-              <Form onSubmit={this.handleSubmit}>
-               <b><Label htmlFor="requestID">Request ID</Label></b>
-                 <Input id="requestID" name="requestID" type="text" placeholder="Enter Request ID here"/>
-                <br></br>
-                 <Label htmlFor="drugName" >Drug Name</Label>
-                 <Input id="drugName" name="drugName" type="text"  placeholder="Enter Drug name here"/>
-                 <br></br>
-                 <Label htmlFor="requestedQuantity">Requesting Quantity</Label>
-                  <Input id="requestedQuantity" name="requestedQuantity" type="text" placeholder="Enter requesting quantity here"/>
-                  <br></br>
-                  <Label htmlFor="availableQuantity">Available Quantity</Label>
-                  <Input id="availableQuantity" name="availableQuantity" type="text" placeholder="Enter available quantity here"/>
-                  <br></br>
-                  <Label htmlFor="department">Department</Label>
-                  <Input id="department" name="department" type="text" placeholder="Enter the relevant department here" />
-                  <br></br>
-                  
-                <Button>Send Request</Button>
-                </Form>
-              </CardBody>
-            
-           </Card>
-          </div>
-        );
-      };
-    
-    }
-
-export default SendRequests; 
-
-
-  
-=======
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -170,4 +113,3 @@ export default SendRequests;
 }
 
 export default SendRequests; 
->>>>>>> 65037f94ecf1b6e3a5434f78aeeb8046c2ce34df
